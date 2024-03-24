1b308fc1cd005f9f9ffbf40a5de3cfc7d38c2b68
Bruno Shiroma <brunoshiroma@gmail.com>
2024-03-24 11:57:51 -0300

    feat: update kotlin version, fix: webpack-dev-middleware version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    alias(libs.plugins.kotlinMultiplatform)
+
+    alias(libs.plugins.jetbrainsCompose)
 }
 
 group = "com.brunoshiroma"
@@ -13,8 +15,10 @@ repositories {
 }
 
 kotlin {
-    wasm {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs() {
         binaries.executable()
+        moduleName = "kotlin-wasm-poc"
         browser {
             testTask {
                 useKarma {
@@ -23,25 +27,31 @@ kotlin {
                 }
             }
             commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).copy(
-                    open = mapOf(
-                        "app" to mapOf(
-                            "name" to "google-chrome",
-                            "arguments" to listOf("--js-flags=--experimental-wasm-gc")
-                        )
-                    )
-                )
+                outputFileName = "kotlinWasmPoc.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.projectDir.path)
+                    }
+                }
             }
         }
     }
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
         }
-        val wasmMain by getting
-        val wasmTest by getting
     }
+    dependencies {
+        commonMainImplementation(npm("webpack-dev-middleware", "5.3.4"))
+    }
+}
+
+compose.experimental {
+    web.application {}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
 
 rootProject.name = "kotlin-wasm-poc"
 
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
